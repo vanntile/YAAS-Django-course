8e06be1c66e496809a6de824e149e7ad4ce5903d
vanntile <valentin.ionita1201@gmail.com>
2019-09-21 16:30:36 +0300

    REQ2 DONE - Edit account

--- user/utils.py
+++ user/utils.py
@@ -11,3 +11,8 @@ class CreateSignupForm(forms.Form):
 class CreateSigninForm(forms.Form):
     username = forms.CharField(max_length=64)
     password = forms.CharField(widget=widgets.PasswordInput)
+
+
+class CreateEditAccountForm(forms.Form):
+    email = forms.CharField(widget=widgets.EmailInput, required=False)
+    password = forms.CharField(widget=widgets.PasswordInput, required=False)
--- user/views.py
+++ user/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
 
-from user.utils import CreateSignupForm, CreateSigninForm
+from user.utils import *
 
 
 class SignUp(View):
@@ -84,4 +84,33 @@ def signout(request):
 
 
 class EditProfile(View):
-    pass
+    def get(self, request):
+        if request.user.is_authenticated:
+            return render(request, 'profile.html', {'form': CreateEditAccountForm()}, status=200)
+        else:
+            return HttpResponseRedirect(reverse('signin'), status=302)
+
+    def post(self, request):
+        form = CreateEditAccountForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+
+            if cd['email']:
+                try:
+                    User.objects.get(email=cd['email'])
+                    return render(request, 'profile.html', {'form': CreateEditAccountForm()}, status=200)
+                except User.DoesNotExist:
+                    user = User.objects.get(username=request.user)
+                    user.email = cd['email']
+                    user.save()
+
+            if cd['password']:
+                user = User.objects.get(username=request.user)
+                user.set_password(cd['password'])
+                user.save()
+                update_session_auth_hash(request, user)
+
+            return HttpResponseRedirect(reverse('user:user'), status=302)
+
+        return render(request, 'profile.html', {'form': CreateEditAccountForm()}, status=200)
+
